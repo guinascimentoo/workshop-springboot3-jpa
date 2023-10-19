c2f6e2018e1ae957f562aa8d65ddacbe28221964
unknown <guilherme_nascimeento18@outlook.com>
2023-10-19 17:36:35 -0300

    Associação entre Produto e Items de pedido

--- src/main/java/com/portifolio/workshop/entidades/Produto.java
+++ src/main/java/com/portifolio/workshop/entidades/Produto.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +33,9 @@ public class Produto implements Serializable{
 	@ManyToMany
 	@JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<PedidoItem> items = new HashSet<>();
 	
 	public Produto() {
 	}
@@ -85,6 +91,15 @@ public class Produto implements Serializable{
 	
 	public Set<Categoria> getCategorias() {
 		return categorias;
+	}
+	
+	@JsonIgnore
+	public Set<Pedido> getPedido(){
+		Set<Pedido> set = new HashSet<>();
+		for(PedidoItem x : items) {
+			set.add(x.getPedido());
+		}
+		return set;
 	}
 
 	@Override
