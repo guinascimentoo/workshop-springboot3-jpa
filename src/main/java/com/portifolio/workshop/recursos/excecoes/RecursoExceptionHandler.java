60b077806378f473c1a56f9c99503ed561381022
unknown <guilherme_nascimeento18@outlook.com>
2023-10-19 19:40:43 -0300

    Tratamento de exceção delete

--- src/main/java/com/portifolio/workshop/recursos/excecoes/RecursoExceptionHandler.java
+++ src/main/java/com/portifolio/workshop/recursos/excecoes/RecursoExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.portifolio.workshop.servicos.excecoes.DatabaseException;
 import com.portifolio.workshop.servicos.excecoes.RecursoNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class RecursoExceptionHandler {
 	public ResponseEntity<ErroPadrao> recursoNaoEncontrado(RecursoNotFoundException e, HttpServletRequest solicitar){
 		String erro = "Recurso não encontrado";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		ErroPadrao err = new ErroPadrao(Instant.now(), status.value(), erro, e.getMessage(), solicitar.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<ErroPadrao> database(DatabaseException e, HttpServletRequest solicitar){
+		String erro = "Erro base de dados ";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ErroPadrao err = new ErroPadrao(Instant.now(), status.value(), erro, e.getMessage(), solicitar.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/portifolio/workshop/servicos/ServicoUsuario.java
+++ src/main/java/com/portifolio/workshop/servicos/ServicoUsuario.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.portifolio.workshop.entidades.Usuario;
 import com.portifolio.workshop.repositorios.UsuarioRepositorio;
+import com.portifolio.workshop.servicos.excecoes.DatabaseException;
 import com.portifolio.workshop.servicos.excecoes.RecursoNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class ServicoUsuario {
 	}
 	
 	public void delete(Long id) {
-		repositorio.deleteById(id);
+		try {
+			repositorio.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new RecursoNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Usuario atualizacao(Long id, Usuario obj) {
