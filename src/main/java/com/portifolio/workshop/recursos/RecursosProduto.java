a3492d21a7059a4e8cbbfbae3032764ed0bfeccb
unknown <guilherme_nascimeento18@outlook.com>
2023-10-20 18:02:29 -0300

    Inserção, deleção e atualização da Entidade Produto

--- src/main/java/com/portifolio/workshop/recursos/RecursosProduto.java
+++ src/main/java/com/portifolio/workshop/recursos/RecursosProduto.java
@@ -1,13 +1,19 @@
 package com.portifolio.workshop.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.portifolio.workshop.entidades.Produto;
 import com.portifolio.workshop.servicos.ServicoProduto;
@@ -28,6 +34,25 @@ public class RecursosProduto {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Produto> procurarPorId(@PathVariable Long id){
 		Produto obj = servico.procurarPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> inserir(@RequestBody Produto obj){
+		obj = servico.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		servico.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Produto> atualizacao(@PathVariable Long id, @RequestBody Produto obj){
+		obj = servico.atualizacao(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/portifolio/workshop/servicos/ServicoProduto.java
+++ src/main/java/com/portifolio/workshop/servicos/ServicoProduto.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.portifolio.workshop.entidades.Produto;
 import com.portifolio.workshop.repositorios.ProdutoRepositorio;
+import com.portifolio.workshop.servicos.excecoes.DatabaseException;
+import com.portifolio.workshop.servicos.excecoes.RecursoNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ServicoProduto {
@@ -22,5 +28,35 @@ public class ServicoProduto {
 	public Produto procurarPorId(Long id) {
 		Optional<Produto> obj = repositorio.findById(id);
 		return obj.get();
+	}
+	
+	public Produto inserir(Produto obj) {
+		return repositorio.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repositorio.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new RecursoNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Produto atualizacao(Long id, Produto obj) {
+		try {
+			Produto entidade = repositorio.getReferenceById(id);
+			atualizacaoDados(entidade, obj);
+			return repositorio.save(entidade);
+		} catch (EntityNotFoundException e) {
+			throw new RecursoNotFoundException(id);
+		}
+	}
+	public void atualizacaoDados(Produto entidade, Produto obj) {
+		entidade.setNome(obj.getNome());
+		entidade.setDescricao(obj.getDescricao());
+		entidade.setPreco(obj.getPreco());
+		entidade.setImgUrl(obj.getImgUrl());
 	}
 }
